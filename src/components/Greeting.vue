0ac98f6bdc01ec52f6569891af82a11b42580410
droidroot1995 <droidroot1995@gmail.com>
2020-01-19 15:25:07 +0300

    test

---
+++ src/components/Greeting.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="greeting">
+    <h1>{{ msg }}</h1>
+    <div class="hello"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Greeting",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style scoped></style>
